8fd2edda0f2000d446f706440443dae1f773e17b
raya <narayaalifah123@gmail.com>
2026-01-29 22:33:02 +0700

    shopping-cart

--- src/app.js
+++ src/app.js
@@ -34,4 +34,67 @@ document.addEventListener('alpine:init', () => {
         price: 35000 },
         ],
     }));
+
+    Alpine.store('cart', {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem){
+        // check apakah ada barang yang sama di cart
+    const cartItem = this.items.find((item) => item.id === newItem.id);
+
+    // jika belum ada / cart kosong
+    if(!cartItem){
+        this.items.push({...newItem, quantity: 1, total: newItem.price});
+        this.quantity++
+        this.total += newItem.price
+    }  else{
+        // jika barang sudah ada, cek apakah sama dengan yang dicart atau beda
+    this.items = this.items.map((item) => {
+        if(item.id !== newItem.id){
+            return item;
+        }else{
+            item.quantity++
+            item.total = item.price * item.quantity
+            this.quantity++
+            this.total += item.price
+            return item;
+        }
+    })
+}
+        },
+        remove(id){
+            // ambil iitem yang mau diremove berdasarkan id
+            const cartItem = this.items.find((item) => item.id === id);
+             if(cartItem.quantity > 1){
+            // telusuri 1 1
+            this.items = this.items.map((item) =>{
+                if(item.id !== id){
+                    return item;
+                }else{
+            item.quantity--
+            item.total = item.price * item.quantity
+            this.quantity--
+            this.total -= item.price
+            return item;
+                }
+            })
+        }else if(cartItem.quantity === 1){
+            // jika barang sisa 1
+            this.items = this.items.filter((item) => item.id !== id)
+            this.quantity--
+            this.total -= cartItem.price
+        }
+        },
+    });
 });
+
+// Konversi ke rupiah
+
+const rupiah =  (number) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0,
+    }).format(number);
+}
